437d6a9ebdbaed9f8295ca65055094d58746db0d
Shansong Huang <you@example.com>
2023-09-17 20:51:13 +0930

    login

--- login.php
+++ login.php
@@ -2,33 +2,54 @@
     require_once "inc/dbconn.inc.php";
 
     // Add your code here
-    
+    $existUser = false;
+    $existPassword = false;
     
     $username = $_POST["userName"];
     $password = $_POST["userPassword"];
     //$password = md5($_POST["userPassword"]);
 
-    $sql = "SELECT * FROM User where email = '".$username."' and password = '".$password."'";
     
+    $sql = "SELECT * FROM User where email = '".$username."'";
     $result = mysqli_query($conn, $sql);
     if($result)
     {
-        if(mysqli_num_rows($result) > 0)
+        if(mysqli_num_rows($result) > 0)//user found
         {
-            $row = mysqli_fetch_assoc($result);
-            $style = $row['style'];
-            switch($style)
+            $existUser = true;
+            $sql = "SELECT * FROM User where email = '".$username."' and password = '".$password."'";
+            $result = mysqli_query($conn, $sql);
+            if($result)
             {
-                case "STUDENT":
-                    header("location: student.html");
-                    break;
-                case "INSTRUCTOR":
-                    header("location: instructor.html");
-                    break;
-                case "PARENT(QSD)":
-                    header("location: qsd.html");
-                    break;
+                if(mysqli_num_rows($result) > 0)//user and password match
+                {
+                    $existPassword = true;
+                    $row = mysqli_fetch_assoc($result);
+                    $style = $row['style'];
+                    switch($style)
+                    {
+                        case "STUDENT":
+                            header("location: student.html");
+                            break;
+                        case "INSTRUCTOR":
+                            header("location: instructor.html");
+                            break;
+                        case "PARENT(QSD)":
+                            header("location: qsd.html");
+                            break;
+                    }
+                }
+                else
+                {
+                    $existPassword = false;
+                    echo "The password incorrect!<br>The page will be return after 3 seconds.";
+                    header("Refresh:3; login.html");
+                }
             }
+            
+            
+
+            
             
             /*
             while($row = mysqli_fetch_assoc($result))
@@ -39,7 +60,8 @@
         }
         else
         {
-            echo "The user is not Found or password incorrect!<br>The page will be return after 3 seconds.";
+            $existUser = false;
+            echo "The user is not Found!<br>The page will be return after 3 seconds.";
             header("Refresh:3; login.html");
         }
     }
