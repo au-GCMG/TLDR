490b30d0c9c0fab586e7cc70eae8ad050ff61ad6
SHANSONG HUANG <david.sword@outlook.com>
2023-09-20 22:37:15 +0930

    shansong-student-1

--- inc/menu.inc.php
+++ inc/menu.inc.php
@@ -1,6 +1,8 @@
-<ul id="menu">
-    <li><a href="student.php">Overview</a></li>
-    <li><a href="MyLogbook.php">Add</a></li>
-    <li><a href="MyCBT/S.php">History</a></li>
-    <li><a href="MyFinance.php">History</a></li>
-</ul>
+<div id = menuBlock>
+    <ul id="menu">
+        <li><a href="student.php">Overview</a></li>
+        <li><a href="MyLogbook.php">MyLogbook</a></li>
+        <li><a href="MyCBT/S.php">MyCBT/S</a></li>
+        <li><a href="MyFinance.php">MyFinance</a></li>
+    </ul>
+</div>
--- student.php
+++ student.php
@@ -3,15 +3,32 @@
     <meta charset="utf-8" />
     <meta name="author" content="Shansong Huang" />
     <meta name="description" content="TLDR student" />
-    <link rel = "stylesheet", type="text/css", href="styles/signup.css">
-    <script src="scripts/script.js" defer></script>
+    <link rel = "stylesheet", type="text/css", href="styles/student.css">
+    <script src="scripts/studentMenu.js" defer></script>
     <title>Student</title>
   </head>
   <body>
     <?php
+        session_start();
         $email = $_SESSION['email'];
-        echo $email;
+        require_once "inc/dbconn.inc.php";
+        $sql = "SELECT * FROM User where email = '{$email}'";
+        $result = mysqli_query($conn, $sql);
+        $licence = "";
+        $firstname = "";
+        if($result)
+        {
+          if(mysqli_num_rows($result) > 0)
+          {
+            $row = mysqli_fetch_assoc($result);
+            $licence = $row['licence'];
+            $firstname = $row['firstname'];
+            echo "<h2>Welcome $firstname ($licence)</h2>";                       
+            require_once "inc/menu.inc.php";
+          }
+        }
+        mysqli_free_result($result);
+        mysqli_close($conn);
     ?>
-
   </body>
 </html>
